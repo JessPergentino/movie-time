f84e25b995797ff6e0e5daac067d6d89e78f9635
Jessica Pergentino <jessicapergentino@hotmail.com>
2021-09-08 15:18:57 -0300

    Implement React Native Navigation and Routes

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
-const DetailsScreen = () => {
+const DetailsScreen = ( {navigation, route} ) => {
+    console.log(route);
+    const movie = route.params.movie;
+    
     return (
       <View style={styles.mainView}>
-        <Text>Details Screen</Text>
-          <Button
-            title="Go to Image"
-            onPress={() => {console.log("Go to BigImageView")}}/>
+        <Text style={{fontSize: 20}}>{movie.title} ({movie.release})</Text>
+        <Text style={{fontSize: 100}} >{movie.screenNumber}</Text>
+        <Button
+          title="Go to Image"
+          onPress={() => {navigation.navigate("BigImageView")}}/>
+        <Button
+          title="More Details"
+          onPress={() => {
+            movie.screenNumber = movie.screenNumber + 1;
+            console.log(movie);
+            navigation.push("Details_to_Details",
+                            {movie: movie} )}}/>
+        <Button
+          title="Go Back to Home"
+          onPress={() => {navigation.popToTop()}}/>
       </View>
     )
 };
